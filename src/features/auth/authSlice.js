95201ebef51fe7242ac053f203c540c8817098e7
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-01-09 00:32:49 +0600

    firebase config, hosting and redux-toolkit setup added

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    user: {
+        email: "",
+        role: ""
+    },
+    isLoading: true,
+    isError: false,
+    error: ""
+}
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState,
+})
+
+export default authSlice.reducer
