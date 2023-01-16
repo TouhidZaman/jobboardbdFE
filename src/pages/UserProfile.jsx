dd80cd5c7aaadeacc83f8d868b7c2208cff9b8a8
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-01-16 23:23:06 +0600

    action buttons removed from my frofile

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { useGetUserByIdQuery } from "features/users/usersAPI";
@@ -9,6 +9,9 @@ import { useAddChatMutation, useGetChatQuery } from "features/chat/chatAPI";
 const UserProfile = () => {
   const { userId } = useParams();
   const navigate = useNavigate();
+  const location = useLocation();
+  const isMyProfile = location.pathname === "/dashboard";
+
   const { data: selectedUser, isLoading } = useGetUserByIdQuery(userId, {
     skip: !userId,
   });
@@ -69,39 +72,41 @@ const UserProfile = () => {
           <p className="text-gray-700">Junior Front-end Engineer</p>
           <p className="text-sm text-gray-500">{country}</p>
         </div>
-        <div className="flex-1 flex flex-col items-center lg:items-end justify-end px-8 mt-2">
-          <div className="flex items-center space-x-4 mt-2">
-            <button className="flex items-center bg-primary/80 hover:bg-primary text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+        {!isMyProfile && (
+          <div className="flex-1 flex flex-col items-center lg:items-end justify-end px-8 mt-2">
+            <div className="flex items-center space-x-4 mt-2">
+              <button className="flex items-center bg-primary/80 hover:bg-primary text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
+                </svg>
+                <span>Connect</span>
+              </button>
+              <button
+                onClick={() => handleChatStart()}
+                className="flex items-center bg-primary/80 hover:bg-primary text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100"
               >
-                <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
-              </svg>
-              <span>Connect</span>
-            </button>
-            <button
-              onClick={() => handleChatStart()}
-              className="flex items-center bg-primary/80 hover:bg-primary text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span>Message</span>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Message</span>
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
 
       <div className="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
