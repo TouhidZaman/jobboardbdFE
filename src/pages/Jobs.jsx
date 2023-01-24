e68968ffc3eb9af4685bf35536303602a9599e21
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-01-24 12:23:15 +0600

    job search feature added

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -3,12 +3,15 @@ import JobCard from "components/reusable/JobCard";
 import { useGetJobsQuery } from "features/jobs/jobsAPI";
 import Loading from "components/reusable/Loading";
 import { toast } from "react-hot-toast";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { resetError } from "features/auth/authSlice";
+import { clearSearch } from "features/filter/filterSlice";
 
 const Jobs = () => {
+  const { keywords } = useSelector((state) => state.filter);
   const { data, isLoading, isError, error } = useGetJobsQuery();
   const jobsData = data?.data || [];
+  let filteredJobs = [];
   const dispatch = useDispatch();
   useEffect(() => {
     if (isError) {
@@ -20,21 +23,43 @@ const Jobs = () => {
   if (isLoading) {
     return <Loading />;
   }
+
+  //Applying search filtering
+  if (keywords) {
+    let keywordsArray = keywords.split(" ");
+    filteredJobs = jobsData.filter((job) =>
+      keywordsArray.some(
+        (keyword) =>
+          job.position?.toLowerCase().includes(keyword.toLowerCase()) ||
+          job.employmentType?.toLowerCase().includes(keyword.toLowerCase())
+      )
+    );
+  } else {
+    filteredJobs = jobsData;
+  }
   return (
     <div className="pt-14">
       <div className="bg-primary/10 p-5 rounded-2xl">
-        <h1 className="font-semibold text-xl">Find Jobs</h1>
+        <div className="flex justify-between items-center">
+          <h1 className="font-semibold text-xl">
+            Find Jobs{keywords ? `: showing results for "${keywords}"` : ""}
+          </h1>
+          <button
+            className={`btn ${!keywords ? "hidden" : ""}`}
+            onClick={() => dispatch(clearSearch())}
+          >
+            Clear Search
+          </button>
+        </div>
       </div>
-      {jobsData?.length ? (
+      {filteredJobs?.length ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
-          {jobsData.map((job) => (
+          {filteredJobs.map((job) => (
             <JobCard key={job._id} jobData={job} />
           ))}
         </div>
       ) : (
-        <h3 className="text-center text-2xl mt-16">
-          Oops! no jobs added yet, please come back later
-        </h3>
+        <h3 className="text-center text-2xl mt-16">Oops! no jobs found</h3>
       )}
     </div>
   );
